7b65ee6050ccd90460cb53a8b5436b8d7d2ca9cc
Vitalii Chervonyi <vognuk@gmail.com>
2021-10-25 12:09:29 +0300

    Fixed error

--- app.js
+++ app.js
@@ -3,18 +3,10 @@ const logger = require("morgan")
 const cors = require("cors")
 /*Safity*/
 const helmet = require('helmet')
-// const rateLimit = require('express-rate-limit')
-
-// const contactsRouter = require("./routes/contacts")
 const usersRouter = require('./routes/users')
-
 const app = express()
-
 const formatsLogger = app.get("env") === "development" ? "dev" : "short"
-
 app.use(logger(formatsLogger))
-
-// app.use(helmet())
 
 app.use(cors({
   origin: '*',
@@ -24,13 +16,10 @@ app.use(cors({
 }),
 )
 app.use(express.json())
-
 app.use('/api/users', usersRouter)
-// app.use('/api/contacts', contactsRouter)
-
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" })
-});
+})
 
 app.use((err, req, res, next) => {
   const status = err.status || 500
--- model/schemas/user.js
+++ model/schemas/user.js
@@ -31,6 +31,10 @@ const userSchema = new Schema({
         type: String,
         default: 'Guest',    
     },
+    token: {
+        type: String,
+        default: null,
+    },
 },
     {
         versionKey: false,
