9810398db2528291f0941045ec0ce57241bc59f4
Vitalii Chervonyi <vognuk@gmail.com>
2021-11-02 12:55:43 +0300

    Rebuild

--- client/build/static/js/7.a462e93f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonptest-sigma-front-react"]=this["webpackJsonptest-sigma-front-react"]||[]).push([[7],{145:function(t,e,r){"use strict";r.r(e);r(0);var s=r(2);e.default=function(){return Object(s.jsx)(s.Fragment,{children:Object(s.jsx)("h1",{children:"User private page"})})}}}]);
-//# sourceMappingURL=7.a462e93f.chunk.js.map
